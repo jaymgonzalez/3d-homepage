00bf7aade4dc3ab732b447fd6c62f852150574da
Jay M Gonzalez <jaimungon@gmail.com>
2022-10-23 14:47:40 +0200

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
 }
 
-export default App
+function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  )
+}
+
+export default WrappedApp
